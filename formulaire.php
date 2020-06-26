17dc52953b027590410fbdea9f5c25fd160ac9cb
oceanelouerat <62375391+oceanelouerat@users.noreply.github.com>
2020-06-26 10:18:49 +0200

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,25 +1,25 @@
 
 index.php
-<form action="/ma-page-de-traitement" method="post">
+<form action="/ma-page-de-traitement" method="post"> <!--création du balise formulaire-->
     <div>
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="oluser_name">
+        <label for="name">Nom :</label><!--Titre affiché pour l'information demandée-->
+        <input type="text" id="name" name="oluser_name"><!--type d'information et ID de l'information-->
     </div>
     <div>
-        <label for="city">Ville :</label>
-        <input type="text" id="city" name="oluser_city">
+        <label for="city">Ville :</label><!--Titre affiché pour l'information demandée-->
+        <input type="text" id="city" name="oluser_city"><!--type d'information et ID de l'information-->
     </div>
     <div>
-        <label for="phone">Téléphone :</label>
-        <input id="phone" name="oluser_phone">
+        <label for="phone">Téléphone :</label><!--Titre affiché pour l'information demandée-->
+        <input id="phone" name="oluser_phone"><!--type d'information et ID de l'information-->
     </div>
     <div>
-        <label for="mail">e-mail :</label>
-        <input type="email" id="mail" name="oluser_mail">
+        <label for="mail">e-mail :</label><!--Titre affiché pour l'information demandée-->
+        <input type="email" id="mail" name="oluser_mail"><!--type d'information et ID de l'information-->
     </div>
     <div>
-        <label for="project">Votre projet :</label>
-        <input type="select">
+        <label for="project">Votre projet :</label><!--Titre affiché pour l'information demandée-->
+        <input type="select"><!--Choix déroulant de propositions-->
    			<option valeur="siteweb">Création de site web</option>
    			<option valeur="seo">référencement</option>
    			<option valeur="refonte">refonte de site web</option>
